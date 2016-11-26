eb41ce5087ddfbfa50c07eadcb016a2552e703b0
Ubuntu <ubuntu@ip-172-31-59-223.ec2.internal>
2016-11-26 22:02:17 +0000

    better facetags

--- fetchtags.js
+++ fetchtags.js
@@ -10,31 +10,15 @@ fs.readFile('files.txt', 'utf8', function (err,list) {
       name : file.Key.slice(0, file.Key.length-4),
     };
     let link = url + fileObj.name + '.jpg';
-    console.log(link);
-    // cloudVision(link)
-    // .then((tags) => {
-    //   fileObj.tags = tags;
-    //   data.push(fileObj);
-    //   fs.writeFile("tags.txt", JSON.stringify(data), function(err) {
-    //     console.log(data);
-    //   });
-    // });
+    cloudVision(link)
+    .then((tags) => {
+	console.log(tags)
+
+       fileObj.tags = tags;
+       data.push(fileObj);
+       fs.writeFile("tags.txt", JSON.stringify(data), function(err) {
+         console.log(data);
+       });
+    });
   });
 });
-  // let url = 'https://s3.eu-central-1.amazonaws.com/gifrun/'
-  // list.forEach((file) => {
-  //   let fileObj = {
-  //     name : file.Key.slice(0, file.Key.length-3)
-  //   };
-  //   cloudVision(url + file.Key.slice(0, file.Key.length-3) + 'jpg')
-  //   .then((tags) => {
-  //     fileObj.tags = tags;
-  //     data.push(fileObj);
-  //     fs.writeFile("tags.txt", data, function(err) {
-  //         if(err) {
-  //             return console.log(err);
-  //         }
-  //     });
-  //     console.log(data);
-  //   });
-  // });

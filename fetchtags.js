bb3d8b1ce465b44bd86571b0f80cd0d89bb5a6cd
Ubuntu <ubuntu@ip-172-31-59-223.ec2.internal>
2016-11-26 23:47:53 +0000

    Final tags

--- fetchtags.js
+++ fetchtags.js
@@ -3,24 +3,31 @@ const cloudVision = require('./server/cloudvision/cloudvision');
 var fs = require('fs');
 
 const data = [];
+const newData = [];
+let i = 0;
 fs.readFile('files.txt', 'utf8', function (err,list) {
   let url = 'https://s3.eu-central-1.amazonaws.com/gifrun/'
   JSON.parse(list).forEach((file)=>{
     let name = file.Key.slice(0, file.Key.length-4);
     let link = url + name + '.jpg';
-    console.log(link);
-    cloudVision(link)
-    .then((tags) => {
-      let fileObj = {
-        name : name,
-        tags: tags
-      };
-      data.push(fileObj);
-      fs.writeFile("tags.txt", JSON.stringify(data), function(err) {
-        console.log(data);
-      });
+    data.push({
+        name : file.Key.slice(4, file.Key.length-4),
+        link: link
     });
   });
+
+  let i = 0;
+  setInterval(() => {
+    cloudVision(data[i].link)
+    .then((tags) => {
+      newData.push({
+        name: data[i].name,
+        tags: tags
+      });
+      fs.writeFile("tags.txt", JSON.stringify(newData), function(err) {
+        console.log(JSON.stringify(newData));
+      });
+    });
+    i += 1;
+  }, 2000);
 });
-
-

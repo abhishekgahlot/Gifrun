a81b19b3ad193d6db8f073ea5e7b8e05475298f9
AbhishekGahlot <me@abhishek.it>
2016-11-27 13:37:01 +0530

    final changes with tags and classification

--- server/files/syncfiles.js
+++ server/files/syncfiles.js
@@ -5,6 +5,19 @@ const dbw = require('../db/wrapper').dbw;
 const tableName = table.name;
 const tableIndex = table.secondaryIndexField;
 
+var fs = require('fs');
+let tags = [];
+fs.readFile('tags.txt', 'utf8', function (err,list) {
+  tags = JSON.parse(list);
+});
+
+function getTags(tags, name) {
+  return tags.filter((a) => {
+    return a.name == name;
+  }).shift();
+}
+
+
 function syncfiles() {
   return new Promise((resolve, reject) => {
     let list = [];
@@ -16,8 +29,13 @@ function syncfiles() {
           name: file.Key.replace('img/','').replace('.gif', ''),
           size: file.Size,
           ETag: file.ETag,
-          LastModified: file.LastModified.toString()
+          LastModified: file.LastModified.toString(),
         }
+
+        let tagsData = getTags(tags, filedb.name);
+        filedb.tags = tagsData.tags;
+        filedb.customTags = tagsData.custom_tags || [];
+
         dbw.find(tableName, { [tableIndex]: filedb.name })
         .then((data) => {
           if (!data.length) {
--- test.js
+++ test.js
@@ -6,7 +6,26 @@ let list = [];
 // });
 
 
-cloudVision('https://s3.eu-central-1.amazonaws.com/gifrun/img/005157a4fe997830c2c468abef20a0b7.jpg')
+cloudVision('https://s3.eu-central-1.amazonaws.com/gifrun/img/d4a2f3c1c731f0abf581e31e1b6fcd55.jpg')
 .then((data) => {
   console.log(data);
 })
+
+// 683a4f51b62abd9a4563de4e144c74f9
+
+// var fs = require('fs');
+// let data = [];
+// fs.readFile('tags.txt', 'utf8', function (err,list) {
+//   data = list;
+//   x = JSON.parse(data).filter((a)=> {
+//     return a.name === '005157a4fe997830c2c468abef20a0b7'
+//   });
+//   console.log(JSON.stringify(x));
+// });
+
+// {"name":"5c66ad9b0f95c850caaa1c518794f8b4",
+// "tags":[ { desc: 'image', mid: '/m/0jg24', score: 90.214467 },
+//   { desc: 'furniture', mid: '/m/0c_jw', score: 75.198758 },
+//   { desc: 'cat', mid: '/m/01yrx', score: 72.08563099999999 },
+//   { desc: 'art', mid: '/m/0jjw', score: 67.85562 },
+//   { desc: 'bed', mid: '/m/03ssj5', score: 64.244878 } ]}

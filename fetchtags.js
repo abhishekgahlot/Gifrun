052068ba004fb7d1befdcd84a52ec53e780dcaf6
AbhishekGahlot <me@abhishek.it>
2016-11-27 16:09:59 +0530

    changes to fetchtags

--- fetchtags.js
+++ fetchtags.js
@@ -4,7 +4,30 @@ var fs = require('fs');
 
 const data = [];
 const newData = [];
-let i = 0;
+
+function getTags(newData, data) {
+  let deferred = Promise.defer();
+  let fileobj = data.shift();
+  cloudVision(fileobj.link)
+  .then((tags) => {
+    newData.push({
+      name: fileobj.name,
+      tags: tags
+    });
+    console.log(newData)
+    if (newData.length == 2522) {
+      deferred.resolve();
+    } else {
+      getTags(newData, data).then(()=>{
+        deferred.resolve();
+      });
+    }
+  });
+
+  return deferred.promise;
+}
+
+
 fs.readFile('files.txt', 'utf8', function (err,list) {
   let url = 'https://s3.eu-central-1.amazonaws.com/gifrun/'
   JSON.parse(list).forEach((file)=>{
@@ -15,20 +38,14 @@ fs.readFile('files.txt', 'utf8', function (err,list) {
         link: link
     });
   });
-  console.log(data.length);
-  // let i = 0;
-  // setInterval(() => {
-  //   cloudVision(data[i].link)
-  //   .then((tags) => {
-  //     newData.push({
-  //       name: data[i].name,
-  //       tags: tags
-  //     });
-  //     fs.writeFile("newtags.txt", JSON.stringify(newData), function(err) {
-  //     });
-  //   });
-  //   i += 1;
-  // }, 1500);
+  
+  getTags(newData, data)
+  .then(()=>{
+    console.log(newData);
+    fs.writeFile("newtags.txt", JSON.stringify(newData), function(err) {
+    });
+  });
+
 });
 // let list = [];
 // syncdb.getAllFiles(list).then((data)=>{
